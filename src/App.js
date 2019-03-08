c263db5a7b58f6fade9d6244c2fc164adea6d179
Ronald Hajewski <ronald.hajewski@gmail.com>
2019-03-07 20:11:04 -0600

    completed props exercise

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ console.log(ClassRoom);
 
 /* eslint-disable no-unused-vars, no-console */
 // function App(props) {
-
 const product = {
   name: "ajax",
   description: "Cleans your bathroom real good"
@@ -30,13 +29,13 @@ const App = () => {
       {/* pass SuperComponent the string: "hello word" in a prop called "message"*/}
       <SuperComponent message="hello world" />
       {/* pass SuperComponent the string: "goodbye world" in a prop called "message" */}
-
+      <SuperComponent message="goodbye world" />
       {/* pass SuperComponent the string: "Props are awesome world" in a prop called "message" */}
-
+      <SuperComponent message="Props are awesome world" />
       {/* pass SuperComponent the number: 42 in a prop called "magicNumber" */}
-
+      <SuperComponent magicNumber={42} />
       {/* pass SuperComponent the number: 21 in a prop called "magicNumber" */}
-
+      <SuperComponent magicNumber={21} />
       {/*
         What have we seen here? This "SuperComponent" takes multiple 
         props and simply displays the content for whichever prop was passed in. 
@@ -45,19 +44,19 @@ const App = () => {
       */}
 
       {/* pass the Students component an array of names in a prop called "names" */}
-
+      <Students names={names} />
       {/* 
         Go into the Students component and take a look at is. What's it doing?
         Try using the Students component again without the names prop. What happens? 
         Delete this one after
       */}
-
+      {/* <Students /> */}
       {/*
         Go into the Students component and add support for an optional prop
         called "groupName". Send a string of your choice and display it above 
         the list of students
       */}
-
+      <Students names={names} groupName="hey there this is a groupName" />
       {/*
         The Students component should have showed us how to use props that 
         are optional and required, as well as practice passing multiple props.
@@ -65,8 +64,12 @@ const App = () => {
       */}
 
       {/* pass the ClassRoom component the string "Class Number 1" in a prop called className */}
-
+      <ClassRoom className={["Class Number 1"]} />
       {/* render the Students component inside the ClassRoom component; pass Students as a child */}
+      <ClassRoom
+        className={["Class Number 2"]}
+        children={<Students names={names} />}
+      />
     </div>
   );
 };
--- src/components/Students.js
+++ src/components/Students.js
@@ -1,9 +1,10 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const Students = (props) => {
+const Students = props => {
   return (
     <div>
+      <h1>{props.groupName}</h1>
       <h3>The students are:</h3>
       {props.names.map((name, index) => (
         <p key={index}>{name}</p>
@@ -12,8 +13,15 @@ const Students = (props) => {
   );
 };
 
+/*
+        Go into the Students component and add support for an optional prop
+        called "groupName". Send a string of your choice and display it above 
+        the list of students
+      */
+
 Students.propTypes = {
-  names: PropTypes.array.isRequired
+  names: PropTypes.array.isRequired,
+  groupName: PropTypes.string
 };
 
 export default Students;
